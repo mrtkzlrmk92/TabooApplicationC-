e5f71b0ba526448de510d02c4da1117260d7086f
Mert KIZILIRMAK <114242955+mrtkzlrmk92@users.noreply.github.com>
2023-01-24 12:44:07 +0300

    TabooApplicationC-

---
+++ tabuNedimÖzturan/Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace tabu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Location = new System.Drawing.Point(55, 426);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Başla";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(226, 224);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(507, 131);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(378, 344);
+            this.listBox1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.progressBar1.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.progressBar1.Location = new System.Drawing.Point(252, 20);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(315, 35);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(340, 60);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(123, 228);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Kelime :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(588, 108);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Yasak Kelimeler";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(190, 34);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Süre :";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Green;
+            this.button2.Location = new System.Drawing.Point(75, 280);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 35);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Doğru";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(262, 278);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 35);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Yanlış";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(128, 520);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 20);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Doğru :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(206, 520);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(18, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(123, 568);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(60, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Yanlış :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(206, 568);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 20);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "0";
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Orange;
+            this.button4.Location = new System.Drawing.Point(166, 362);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 35);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Pas";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(172, 131);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(168, 46);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "A Grubu";
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button5.Location = new System.Drawing.Point(262, 426);
+            this.button5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 35);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Bitir";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ tabuNedimÖzturan/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tabu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] kelime = new string[21];
+        string[,] yasak = new string[21, 3];
+        Random rnd = new Random();
+        int sayac = 0;
+        int dogru = 0;
+        int yanlıs = 0;
+        int pas = 0;
+        int aPuan = 0;
+        int bPuan = 0;
+        bool grup = true;
+        bool basla = true;
+        private void button1_Click(object sender, EventArgs e)
+        {  
+            
+            if (basla == false)
+            {
+                timer1.Stop();
+                button1.Text = "devam Et";
+                basla = true;
+            }
+            else
+            {
+                timer1.Start();
+                button1.Text = "durdur";
+                basla = false;
+            }
+          
+        
+
+            kelime[0] = "HECE";
+            kelime[1] = "KÜSMEK";
+            kelime[2] = "AVİZE";
+            kelime[3] = "KOORDİNAT";
+            kelime[4] = "TERİM";
+            kelime[5] = "SOYUT";
+            kelime[6] = "BASKÜL";
+            kelime[7] = "HALÜSİNASYON";
+            kelime[8] = "ARMAĞAN";
+            kelime[9] = "ANTİKA";
+            kelime[10] = "ADEM ELMASI";
+
+            kelime[11] = "SATRANÇ";
+            kelime[12] = "PARAŞÜT";
+            kelime[13] = "TİYATRO";
+            kelime[14] = "DOST";
+            kelime[15] = "ANAHTAR";
+            kelime[16] = "KEDİ";
+            kelime[17] = "BONKÖR";
+            kelime[18] = "FİDAN";
+            kelime[19] = "PRATİK";
+            kelime[20] = "Mesai";
+
+
+
+            yasak[0, 0] = "Kelime";
+            yasak[0, 1] = "Harf";
+            yasak[0, 2] = "Ses";
+
+            yasak[1, 0] = "Darılmak";
+            yasak[1, 1] = "Kızmak";
+            yasak[1, 2] = "Tartışmak";
+
+            yasak[2, 0] = "Lamba";
+            yasak[2, 1] = "Kristal";
+            yasak[2, 2] = "Tavan";
+
+            yasak[3, 0] = "Yer";
+            yasak[3, 1] = "Enlem";
+            yasak[3, 2] = "Boylam";
+
+            yasak[4, 0] = "Kelime";
+            yasak[4, 1] = "Anlam";
+            yasak[4, 2] = "Fatih";
+
+            yasak[5, 0] = "Duyu";
+            yasak[5, 1] = "Algılamak";
+            yasak[5, 2] = "Görünmeyen";
+
+            yasak[6, 0] = "Tartı";
+            yasak[6, 1] = "Kilo";
+            yasak[6, 2] = "Ağır";
+
+            yasak[7, 0] = "Hayal";
+            yasak[7, 1] = "Gerçek";
+            yasak[7, 2] = "Görmek";
+
+            yasak[8, 0] = "Hediye";
+            yasak[8, 1] = "Vermek";
+            yasak[8, 2] = "Almak";
+
+            yasak[9, 0] = "Zengin";
+            yasak[9, 1] = "Eski";
+            yasak[9, 2] = "Tablo";
+
+            yasak[10, 0] = "Erkek";
+            yasak[10, 1] = "Gırtlak";
+            yasak[10, 2] = "Çıkıntı";
+
+            yasak[11, 0] = "Şah-Mat";
+            yasak[11, 1] = "Vezir";
+            yasak[11, 2] = "Piyon";
+
+            yasak[12, 0] = "Uçak";
+            yasak[12, 1] = "Atlamak";
+            yasak[12, 2] = "Uçmak";
+
+            yasak[13, 0] = "Oyuncu";
+            yasak[13, 1] = "Sahne";
+            yasak[13, 2] = "Perde";
+
+            yasak[14, 0] = "Güven";
+            yasak[14, 1] = "Samimi";
+            yasak[14, 2] = "Dürüst";
+
+            yasak[15, 0] = "Kilit";
+            yasak[15, 1] = "Metal";
+            yasak[15, 2] = "Kasa";
+
+            yasak[16, 0] = "Pati";
+            yasak[16, 1] = "Fare";
+            yasak[16, 2] = "Tüy";
+
+            yasak[17, 0] = "Eli Açık";
+            yasak[17, 1] = "Cömert";
+            yasak[17, 2] = "Para";
+
+            yasak[18, 0] = "Ağaç";
+            yasak[18, 1] = "Büyümek";
+            yasak[18, 2] = "Küçük";
+
+            yasak[19, 0] = "Kolay";
+            yasak[19, 1] = "Zeka";
+            yasak[19, 2] = "Çabuk";
+
+            yasak[20, 0] = "saat";
+            yasak[20, 1] = "iş";
+            yasak[20, 2] = "Fazla";
+
+            if (grup == true)
+            {
+                label9.Text = "A Grubu";
+            }
+            else
+            {
+                label9.Text = "B Grubu";
+            }
+            getir();
+            
+     
+
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sayac += 1;
+            progressBar1.Value = sayac;
+            label1.Text = sayac.ToString();
+
+            if (sayac == 100)
+            {
+                timer1.Stop();
+                basla = true;
+
+                if (grup == true)
+                {
+                    aPuan += dogru - yanlıs;
+                    MessageBox.Show("Süreniz doldu Doğru sayınız " + dogru + " yanlış sayınız " + yanlıs,"A Grubu");
+                    MessageBox.Show("B grubuna geçilecektir" ,"Grunp Değişimi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                    grup = false;
+                }
+                else
+                {
+                    bPuan += dogru - yanlıs;
+                    MessageBox.Show("Süreniz doldu Doğru sayınız " + dogru + " yanlış sayınız " + yanlıs, "B Grubu");
+                    MessageBox.Show("A grubuna geçilecektir", "Grup Değişimi",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+
+                    grup = true;
+                }
+                dogru = 0;
+                yanlıs = 0;
+                label6.Text = dogru.ToString();
+                label8.Text = yanlıs.ToString();
+                button1.Text = "Devam Et";
+                sayac = 0;
+            
+                
+            }
+
+
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            dogru++;
+            label6.Text = dogru.ToString();
+            getir();
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            yanlıs++;
+            label8.Text = yanlıs.ToString();
+
+            getir();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            getir();
+        }
+         void getir()
+        {
+            listBox1.Items.Clear();
+            int no = rnd.Next(0, kelime.Length);
+            textBox1.Text = kelime[no];
+            for (int i = 0; i < 3; i++)
+            {
+                listBox1.Items.Add(yasak[no, i]);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (aPuan > bPuan)
+            {
+                MessageBox.Show($"kazanan A  grup puanı {aPuan} kaybeden b grup puanı {bPuan}", "Kazanan A Grubu");
+                
+            }
+            else
+            {
+                MessageBox.Show($"kazanan B  grup puanı {bPuan} kaybeden A grup puanı {aPuan}", "Kazanan B Grubu");
+            }
+        }
+           
+    }
+}
